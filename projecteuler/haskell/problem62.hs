3906dc59870c335935a897b6a94df6a7d05155d2
Amit Matani <amitmatani@gmail.com>
2013-04-06 18:35:56 -0400

    Clean up code

--- projecteuler/haskell/problem62.hs
+++ projecteuler/haskell/problem62.hs
@@ -9,9 +9,8 @@ problem62 :: Integer
 problem62 = problem62' cubes Map.empty
     where
         problem62' (p:pt) cubeMap
-            | (Map.member key cubeMap) &&
-              (length cubeList) == 5        = head $ sort cubeList
-            | otherwise                     = problem62' pt (Map.insert key cubeList cubeMap)
+            | length cubeList == 5  = head $ sort cubeList
+            | otherwise             = problem62' pt (Map.insert key cubeList cubeMap)
             where
                 key = sort (show p)
                 cubeList = p : (fromMaybe [] (Map.lookup key cubeMap))
