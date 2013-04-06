b5b5da36cc23b230724650fdceb3f0be43030159
Amit Matani <amitmatani@gmail.com>
2013-04-05 20:07:57 -0400

    Do problem74

---
+++ projecteuler/haskell/problem74.hs
@@ -0,0 +1,34 @@
+import Data.Char (digitToInt)
+import qualified Data.Map as Map
+
+saved_steps = Map.fromList
+    [(169, 3),
+     (363601, 3),
+     (1454, 3),
+     (871, 2),
+     (45361, 2),
+     (872, 2),
+     (45362, 2)]
+
+factorial :: Int -> Int
+factorial n = foldl (*) 1 [1..n]
+
+recomp :: Int -> Int
+recomp n = foldl (+) 0 [(factorial (digitToInt x)) | x <- show n]
+
+steps :: Int -> Int
+steps x
+    | x == 169          = 3
+    | x == 363601       = 3
+    | x == 1454         = 3
+    | x == 871          = 2
+    | x == 45361        = 2
+    | x == 872          = 2
+    | x == 45362        = 2
+    | n == x            = 1
+    | otherwise         = 1 + (steps n)
+    where n = recomp x
+
+main :: IO ()
+-- This could be sped up by saving the intermediate steps in a hash
+main = print (sum [1 | x <- [1..1000000], (steps x) == 60])
